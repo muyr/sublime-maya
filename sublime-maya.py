194d0d75d9a3e0fa8d211207a89403f909f54b21
Muyr <muyanru345@163.com>
2013-12-09 13:56:53 +0800

    添加了当maya没有设置断开时，发送代码弹出提示

--- sublime-maya.py
+++ sublime-maya.py
@@ -1,17 +1,30 @@
-import socket
-import sublime, sublime_plugin
+# -*- coding: utf-8 -*-
+###################################################################
+# Author: Mu yanru
+# Date  : 2013.12
+# Email : muyanru345@163.com
+###################################################################
 
-class MayaSelectionCommand(sublime_plugin.TextCommand):  
+import sublime, sublime_plugin
+import socket
+
+class MayaCommand(sublime_plugin.TextCommand):  
     def run(self, args): 
         view = self.view 
         pythonCode = view.substr(view.sel()[0])
-        # sublime.message_dialog("generate is success!")
-        print pythonCode
-        tmpPort = 6001
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        try:
-            s.connect(('127.0.0.1', tmpPort))
-        except:
-            return
-        s.send(pythonCode)
-        s.close()
+        if pythonCode:
+            sendCode(pythonCode)
+        else:
+            allRegion = sublime.Region(0, view.size())
+            sendCode(view.substr(allRegion))
+
+def sendCode(pythonCode):
+    tmpPort = 8888
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    try:
+        s.connect(('127.0.0.1', tmpPort))
+    except:
+        sublime.message_dialog("Maya CommandPort does\'t connect!")
+        return
+    s.send(pythonCode)
+    s.close()
